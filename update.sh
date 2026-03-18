55bd95eca8de21e328381ef8c98b39791c8fb112
not-pro-laptop <copsandrobbers92@gmail.com>
2026-03-18 10:48:48 -0700

    Added tiny comment in update.sh

--- update.sh
+++ update.sh
@@ -1,3 +1,4 @@
+# This keeps the projects up to date by cleaning and cloning
 rm -rf Briefly Briefly-back-end
 git clone "https://github.com/SR-programmar/Briefly.git"
 git clone "https://github.com/SR-programmar/Briefly-back-end.git"
